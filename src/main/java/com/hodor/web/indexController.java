95c5d4c8d3793d05c63c0123d5b172588570493f
hodor <215338572@qq.com>
2021-01-11 23:25:13 +0800

    static resources and exception pages 静态页面和错误页面

--- src/main/java/com/hodor/web/indexController.java
+++ src/main/java/com/hodor/web/indexController.java
@@ -14,15 +14,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class indexController {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name){
+    @GetMapping("/")
+    public String index(){
 //        int i = 9 / 0;
-        String blog = null;
+//        String blog = null;
         //总的异常处理需要放行这个异常NotFoundException
 //        if(blog == null) {
 //            throw new NotFoundException("博客页面不存在");
 //        }
         System.out.println("------------index----------------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog(){
+        System.out.println("------------blog----------------");
+        return "blog";
     }
 }
